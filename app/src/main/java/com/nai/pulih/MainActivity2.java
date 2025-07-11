9ef558b34722ac1bc07ec5abe38a4f67ea4b0e20
azmil3gm <azmilhanif@threegmedia.com>
2025-07-11 14:47:55 +0800

    Added one more page

--- app/src/main/java/com/nai/pulih/MainActivity2.java
+++ app/src/main/java/com/nai/pulih/MainActivity2.java
@@ -1,10 +1,6 @@
 package com.nai.pulih;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,36 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button button;
-    TextView text;
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        button = findViewById(R.id.button);
-        text = findViewById(R.id.textView);
-
-        button.setOnClickListener(v -> {
-            Intent intent = new Intent(this, MainActivity2.class);
-            startActivity(intent);
-        });
-
-        text.setText("TEST");
     }
-
-    public void sendMessage(View view)
-    {
-        System.out.println("=== HELLO WORLD!!");
-    }
-
 }
