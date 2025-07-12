55d8dd8c38b35a90da8bf4cfb0170a5511c07c37
azmil3gm <azmilhanif@threegmedia.com>
2025-07-12 22:08:51 +0800

    chorse: more functionalities added

--- app/src/main/java/com/nai/pulih/MainActivity2.java
+++ app/src/main/java/com/nai/pulih/MainActivity2.java
@@ -1,24 +1,50 @@
 package com.nai.pulih;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.nai.pulih.MainActivity;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.nav_home) {
+                    // Navigate to MainActivity
+                    Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                    startActivity(intent);
+                    return true;
+                } else if (id == R.id.nav_therapist) {
+                    Toast.makeText(MainActivity2.this, "Therapist clicked", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (id == R.id.nav_selfcare) {
+                    Toast.makeText(MainActivity2.this, "Self Care clicked", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (id == R.id.nav_journal) {
+                    Toast.makeText(MainActivity2.this, "Journal clicked", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+
+                return false;
+            }
         });
     }
 }
